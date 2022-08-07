e473bd930e0fa4ff247754c6ac0247bfb00da6e1
shivam agarwal <shivamagarwaloff@gmail.com>
2022-08-08 03:42:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('..//flyravel-backend/db.json');
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
   res.send(db);
